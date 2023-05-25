a41db278cb35f399dfbf31cb10f09e7a43687f10
topujss <adahmedt@gmail.com>
2023-05-24 23:33:06 -0400

    ig with redux core

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/signup',
+    element: 'sign up page',
+  },
+]);
+
+export default router;
